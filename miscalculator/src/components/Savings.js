dd1c30c822df98e8685f3d013d3e6849f6a05ab9
Niranjan Bommidi <niranjanprasad.b@gmail.com>
2018-09-12 09:52:33 +0530

    Added styles for FindWaysToSave

--- miscalculator/src/components/Savings.js
+++ miscalculator/src/components/Savings.js
@@ -12,7 +12,7 @@ class Savings extends Component {
           <SpendingSlider sliderLabel="Bills"/>
           <SpendingSlider sliderLabel="General spending"/>
           <div className="TotalSavings">This means you are saving xyz per month</div>
-          <div className="FindWays">Find ways to save</div>
+          <div className="FindWaysContainer"><div className="FindWays">Find ways to save</div></div>
           <div className="ThumbsUpDown">Was this helpful?</div>
         </div>
     );
